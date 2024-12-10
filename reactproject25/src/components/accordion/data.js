ae4e585829cc893677f26e78d140de24ed35cafb
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-10 00:38:42 -0800

    acccordion component setup

---
+++ reactproject25/src/components/accordion/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answer: "The capital of France is Paris."
+    },
+    {
+      id: 2,
+      question: "How many continents are there?",
+      answer: "There are seven continents on Earth."
+    },
+    {
+      id: 3,
+      question: "What is the largest ocean?",
+      answer: "The Pacific Ocean is the largest ocean."
+    },
+    {
+      id: 4,
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      answer: "Harper Lee wrote 'To Kill a Mockingbird'."
+    },
+    {
+      id: 5,
+      question: "What is the speed of light?",
+      answer: "The speed of light is approximately 299,792 kilometers per second."
+    }
+  ];
+
+  export default data;
