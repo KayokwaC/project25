3fb95bf6b0e15b5f07b69209ff87823593bfaa94
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-10 22:30:45 -0800

    update state and use it to control visibility of answers

--- reactproject25/src/components/accordion/index.jsx
+++ reactproject25/src/components/accordion/index.jsx
@@ -5,16 +5,23 @@ import data from './data';
 export default function Accordion() {
 const [selected, setSelected] = useState(null);
 
+function handleSingleSelection(currentId){
+    setSelected(currentId);
+}
+
 return <div className='wrapper'>
     <div className='accordion'>
         {
-            data && data.length>0? (
+            data?.length>0? (
                 data.map((item, index) => (
                     <div className='item'>
-                        <div className='title'>
+                        <div onClick={()=>handleSingleSelection(item.id)} className='title'>
                             <h3>{item.question}</h3>
                             <span>+</span>
                         </div>
+                        {selected === item.id? <div className='content'>
+                            <div>{item.answer}</div>
+                        </div>:null}
                         </div>
                 
             ))) :(<div>No data found</div>) 
