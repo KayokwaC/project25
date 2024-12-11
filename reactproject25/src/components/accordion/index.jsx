4bf98362bef7ece5d981ecea804c4950b3fc2ab9
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-10 23:36:25 -0800

    add multiselection support

--- reactproject25/src/components/accordion/index.jsx
+++ reactproject25/src/components/accordion/index.jsx
@@ -5,25 +5,48 @@ import './style.css';
 
 export default function Accordion() {
 const [selected, setSelected] = useState(null);
+const [multiSelectEnabled, setMultiSelectEnabled] = useState(false);
+const [selectedItems, setSelectedItems] = useState([]);
+
 
 function handleSingleSelection(currentId){
     // nullify the selected state if the currentId is the same as the selected
     setSelected(currentId === selected? null: currentId);
 }
 
+function handleMultiSelection(currentId){
+
+    if(!multiSelectEnabled){
+        return;
+    }
+    if(selectedItems.length === 0){
+        setSelectedItems([currentId]);
+        return;
+    }
+    // nullify the selected state if the currentId is the same as the selected
+    selectedItems.includes(currentId)? setSelectedItems(selectedItems.filter((id)=> id !== currentId)): setSelectedItems([...selectedItems, currentId]);
+  
+}
+
+function enableMultiSelection(value){
+    setMultiSelectEnabled(value);
+    setSelectedItems([]);
+    setSelected(null);
+}
+
 return <div className='wrapper'>
     <h1>Accordion</h1>
-    <button>Enable Multi Selection</button>
+    <button onClick={()=>enableMultiSelection(!multiSelectEnabled)}>Enable Multi Selection</button>
     <div className='accordion'>
         {
             data?.length>0? (
                 data.map((item, index) => (
                     <div className='accordion-item'>
-                        <div onClick={()=>handleSingleSelection(item.id)} className='title'>
+                        <div onClick={multiSelectEnabled?(()=>handleMultiSelection(item.id)):(()=>handleSingleSelection(item.id))} className='title'>
                             <h3>{item.question}</h3>
                             <span>+</span>
                         </div>
-                        {selected === item.id? <div className='content'>
+                        {(selected === item.id || selectedItems.includes(item.id))? <div className='content'>
                             <div>{item.answer}</div>
                         </div>:null}
                         </div>
