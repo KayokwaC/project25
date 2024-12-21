3118322a4ce5f0408d19f0d35aaa0bd7d5e0cb50
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-21 00:11:28 -0800

    finished without styling

--- reactproject25/src/components/tree-view/menu-item.jsx
+++ reactproject25/src/components/tree-view/menu-item.jsx
@@ -1,16 +1,29 @@
 import MenuList from "./menu-list";
-import {useState} from 'react';
+import { useState } from "react";
+
 export default function MenuItem({ item }) {
-    const [displayChildren, setDisplayChildren] = useState({});
+  const [displayChildren, setDisplayChildren] = useState({});
+
+  function handleToggleChildren(label) {
+    setDisplayChildren({
+      ...displayChildren,
+      [label]: !displayChildren[label],
+    });
+  }
+
   return (
     <li>
-      <div style={{display: 'flex', gap: '20px'}}>
-      <p> {item.label}</p>
-      {
-        (item?.children && item.children.length>0) ? (<span>+</span>): null
-      }
+      <div style={{ display: "flex", gap: "20px" }}>
+        <p> {item.label}</p>
+        {item?.children && item.children.length > 0? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayChildren[item.label] ? "-" : "+"}
+          </span>
+        ) : null}
       </div>
-      {item.children && item.children.length>0 ? (<MenuList list={item.children} />): null}
+      {item.children && item.children.length > 0 && displayChildren[item.label]? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
   );
 }
