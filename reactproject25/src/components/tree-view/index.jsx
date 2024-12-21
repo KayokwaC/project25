54bf3f65f99272e3e2b8ef27d336cebdaf181b69
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-21 00:27:36 -0800

    style adde, tree-view complete

--- reactproject25/src/components/tree-view/index.jsx
+++ reactproject25/src/components/tree-view/index.jsx
@@ -3,7 +3,6 @@ import MenuList from "./menu-list";
 export default function TreeView({menus = []}) {
   return (
     <div className="tree-view-container">
-      <h1>TreeView</h1>
       <MenuList list={menus}/>
     </div>
   );
--- reactproject25/src/components/tree-view/menu-item.jsx
+++ reactproject25/src/components/tree-view/menu-item.jsx
@@ -1,5 +1,6 @@
 import MenuList from "./menu-list";
 import { useState } from "react";
+import {FaMinus, FaPlus} from 'react-icons/fa';
 
 export default function MenuItem({ item }) {
   const [displayChildren, setDisplayChildren] = useState({});
@@ -12,12 +13,12 @@ export default function MenuItem({ item }) {
   }
 
   return (
-    <li>
-      <div style={{ display: "flex", gap: "20px" }}>
+    <li >
+      <div className="menu-item">
         <p> {item.label}</p>
         {item?.children && item.children.length > 0? (
           <span onClick={() => handleToggleChildren(item.label)}>
-            {displayChildren[item.label] ? "-" : "+"}
+            {displayChildren[item.label] ?<FaMinus color="#fff" size={25}/> : <FaPlus color="#fff" size={25}/>}
           </span>
         ) : null}
       </div>
