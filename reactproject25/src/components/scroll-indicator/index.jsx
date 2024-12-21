db4d2955d7c62991ed826d6fe97e2ab9c8bb1a6b
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-21 14:50:08 -0800

    fix build errors

--- reactproject25/src/components/scroll-indicator/index.jsx
+++ reactproject25/src/components/scroll-indicator/index.jsx
@@ -1,7 +1,7 @@
 export default function ScrollIndicator() {
     return (
         <div className="scroll-indicator">
-            <div className="scroll-indicator__bar"></div>
+            <div className="scroll-indicator__bar"> Scroll Indicator</div>
         </div>
     );
 }
