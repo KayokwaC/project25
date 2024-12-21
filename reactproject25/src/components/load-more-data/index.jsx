df41601f2cb0618ba2a6189678971168e9a89d1e
Kayokwa Chibuye <nichibuy@microsoft.com>
2024-12-20 22:20:45 -0800

    laod data and add style

--- reactproject25/src/components/load-more-data/index.jsx
+++ reactproject25/src/components/load-more-data/index.jsx
@@ -1,15 +1,61 @@
-
-
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
 export default function LoadMoreData() {
-    const [loading, setLoading] = useState(false);
-    const [products, setProducts] = useState([]);
-    const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div className="container">
-            <button className="load-more-data__button">Load More</button>
-        </div>
-    )
+   async function fetchProducts() {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const data = await response.json();
+      if (data?.products.length) {
+        setProducts([...products, ...data.products]);
+        setLoading(false);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error:", error);
+    }
+  }
+
+  useEffect(() => {
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <h1>Products</h1>
+      <div className="product-container">
+        {
+
+        products?.length ? (
+          products.map((product, index) => (
+            <div key={index} className="product">
+              <img src={product.thumbnail} alt={product.title} />
+                <p>{product.title}</p>
+            </div>
+          ))
+        ) : (
+          <p>No products found</p>
+        )
+        
+        }
+      </div>
+
+      <button className="load-more-data__button">Load More Products</button>
+    </div>
+  );
 }
